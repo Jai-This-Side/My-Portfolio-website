9b2fc4ae783bbda6a39b124f56b24939458927e2
Jai Singh Bisht <jaisinghprsnl99@gmail.com>
2026-07-03 18:26:59 +0530

    Setup portfolio dependencies and project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 
-export default function App() {
+function App() {
   return (
     <>
       <Navbar />
@@ -9,3 +9,5 @@ export default function App() {
     </>
   );
 }
+
+export default App;
